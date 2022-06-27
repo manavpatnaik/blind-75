63ccb7e76a596779b1dcf9db7cedda20eb5cf0c9
Manav Patnaik <dev.manavpatnaik@gmail.com>
2022-06-27 09:48:39 +0530

    DP problems

---
+++ 24_decode_ways.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int numDecodings(string s) {
+    int n = s.length();
+    vector<int> dp(n, 0);
+    dp[0] = s[0] != '0' ? 1 : 0;
+    for (int i = 1; i < n; i++) {
+        if (s[i-1] == '0' && s[i] == '0') {
+            dp[i] = 0;
+        } else if (s[i-1] != '0' && s[i] == '0') {
+            if (s[i-1] == '1' || s[i-1] == '2') {
+                dp[i] = i >= 2 ? dp[i-2] : 1;
+            } else {
+                dp[i] = 0;
+            }
+        } else if (s[i-1] == '0' && s[i] != '0') {
+            dp[i] = dp[i-1];
+        } else {
+            if (stoi(s.substr(i-1, 2)) <= 26) {
+                dp[i] = dp[i-1] + (i >= 2 ? dp[i-2] : 1);
+            } else {
+                dp[i] = dp[i-1];
+            }
+        }
+    }
+    return dp[n-1];
+}
